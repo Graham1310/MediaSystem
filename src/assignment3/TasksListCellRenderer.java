907afc4a1fa4c5ffb9fc750d5c6ff1333de9f1e9
Marcin <neverborn.store@gmail.com>
2014-02-04 02:29:54 +0000

    paste conflicted files

---
+++ src/assignment3/TasksListCellRenderer.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment3;
+
+import java.awt.Component;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JList;
+
+/**
+ *
+ * @author Neverborn
+ */
+public class TasksListCellRenderer extends DefaultListCellRenderer{
+                public Component getListCellRendererComponent(JList<?> list,
+                                 Object value,
+                                 int index,
+                                 boolean isSelected,
+                                 boolean cellHasFocus) {
+        super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        if (value instanceof Task) {
+            Task task = (Task)value;
+            setText(task.getTaskName() + "   Status:" + task.getStatus());
+        }
+        
+       
+        return this;
+    }    
+}
